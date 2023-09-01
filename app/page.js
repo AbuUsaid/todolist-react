1a4dcad673b7144ba8c09f01cfae578f8dc690ee
AbuUsaid <usaidabu321@gmail.com>
2023-09-01 22:22:54 +0530

    make a heading and style it using tailwind

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
+        Abu's Todo List
+      </h1>
+    </>
+  );
+};
+
+export default page;

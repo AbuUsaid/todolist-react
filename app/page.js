53fdae171874c380dde8bd191b277494c06a1fd8
AbuUsaid <usaidabu321@gmail.com>
2023-09-01 22:46:38 +0530

    add input and button to add task inside form and style them

--- app/page.js
+++ app/page.js
@@ -4,8 +4,23 @@ const page = () => {
   return (
     <>
       <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
-        Abu's Todo List
+        My Todo List
       </h1>
+      <form>
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2 "
+          placeholder="Enter Task here"
+        />
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2 "
+          placeholder="Enter Description here"
+        />
+        <button className="bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5">
+          Add Task
+        </button>
+      </form>
     </>
   );
 };
